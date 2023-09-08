b1aca2350fc369b1759c5dbfaf955a9b7c46db97
DhrumilZalavadia <144266256+DhrumilZalavadia@users.noreply.github.com>
2023-09-08 11:11:01 -0300

    Add files via upload

---
+++ Restaurants.py
@@ -0,0 +1,10 @@
+def main():
+    numberOfPizzeria = 70000
+    percentageOfPizzeria = 12/100 #12%
+    total = numberOfPizzeria/percentageOfPizzeria
+
+    print("No of restaurant: ",total)
+
+if __name__ == "__main__":
+    main()
+
